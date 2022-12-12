3ccd4b273539e424ff821355f15f4ee6fa08ba33
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-12 22:08:14 +0530

    destructured components

--- components/common/index.js
+++ components/common/index.js
@@ -1,2 +1,5 @@
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
+export { default as Hero } from "./Hero";
+export { default as BreadCrumbs } from "./BreadCrumbs";
+
