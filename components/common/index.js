e2d560ccb328a6924366e9138037baedb9cb01c0
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-12 22:25:29 +0530

    Course components destructured

--- components/common/index.js
+++ components/common/index.js
@@ -2,4 +2,5 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as BreadCrumbs } from "./BreadCrumbs";
+export { default as Modal } from "./Modal";
 
