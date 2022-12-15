e49858fdec10741bbbf6ca9eab2402257ea669f8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-15 22:40:32 +0530

    getting single course

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,10 +1,12 @@
 import { Modal } from "@components/common";
+import { getAllCourses } from "@components/content/courses/fetcher";
 import { Curriculum, CourseHero, KeyPoints } from "@components/course";
 import { BaseLayout } from "@components/layout";
 
-export default function Course() {
+export default function Course({ course }) {
 	return (
 		<BaseLayout>
+			{course.title}
 			<div className='py-4'>
 				<CourseHero />
 			</div>
@@ -13,4 +15,26 @@ export default function Course() {
 			<Modal />
 		</BaseLayout>
 	);
+}
+
+export function getStaticPaths() {
+	const { data } = getAllCourses();
+	return {
+		paths: data.map((c) => ({
+			params: {
+				slug: c.slug,
+			},
+		})),
+		fallback: false,
+	};
+}
+
+export function getStaticProps({ params }) {
+	const { data } = getAllCourses();
+	const course = data.filter((c) => c.slug === params.slug)[0];
+	return {
+		props: {
+			course,
+		},
+	};
 }
