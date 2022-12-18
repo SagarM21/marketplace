31b2fc73601b8930a6af439fa2eef91a088860df
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-18 19:51:22 +0530

    connect to metamask

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import { useWeb3 } from "@components/providers";
 import { Hero } from "@components/ui/common";
 import { getAllCourses } from "@components/ui/content/courses/fetcher";
 import { CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 
 function Home({ courses }) {
-	const { web3, isLoading } = useWeb3();
 	return (
 		<>
-			{isLoading
-				? "Is loading web3"
-				: web3
-				? "Web3 ready"
-				: "Please install metamask"}
 			<Hero />
 			<CourseList courses={courses} />
 		</>
