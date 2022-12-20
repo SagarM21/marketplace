34af964149f9f6b8f569c2eed427ad7092faf4a9
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-20 22:17:11 +0530

    loading btn created

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -3,4 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as BreadCrumbs } from "./BreadCrumbs";
 export { default as Modal } from "./Modal";
-
+export { default as Button } from "./Button";
