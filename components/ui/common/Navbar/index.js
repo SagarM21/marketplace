0fa41d4be2887f10d9ae939b927246622bd1a9ec
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-20 22:27:40 +0530

    disable btn when loading

--- components/ui/common/Navbar/index.js
+++ components/ui/common/Navbar/index.js
@@ -4,12 +4,11 @@ import { Button } from "@components/ui/common";
 
 export default function Navbar() {
 	const { connect, isLoading, isWeb3Loaded } = useWeb3();
-
 	return (
 		<section>
 			<div className='relative pt-6 px-4 sm:px-6 lg:px-8'>
 				<nav className='relative' aria-label='Global'>
-					<div className='flex justify-between'>
+					<div className='flex justify-between items-center'>
 						<div>
 							<Link href='/' legacyBehavior>
 								<a className='font-medium mr-8 text-gray-500 hover:text-gray-900'>
@@ -34,11 +33,19 @@ export default function Navbar() {
 								</a>
 							</Link>
 							{isLoading ? (
-								<Button onClick={connect}>Connecting</Button>
+								<Button onClick={connect} disabled={true}>
+									Connecting
+								</Button>
 							) : isWeb3Loaded ? (
 								<Button onClick={connect}>Connect</Button>
 							) : (
-								<Button onClick={connect}>Install Metamask</Button>
+								<Button
+									onClick={() =>
+										window.open("https://metamask.io/download/", "_blank")
+									}
+								>
+									Install Metamask
+								</Button>
 							)}
 						</div>
 					</div>
