baffdcf21f1198864fc15bb2ce9ebfaa56b7c1e3
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-29 17:48:14 +0530

    Active link

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -4,3 +4,4 @@ export { default as Hero } from "./Hero";
 export { default as BreadCrumbs } from "./BreadCrumbs";
 export { default as Modal } from "./Modal";
 export { default as Button } from "./Button";
+export { default as ActiveLink } from "./link";
