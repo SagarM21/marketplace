1da80389d1dd57c75b54cf070b59af5098cfa2d2
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-29 17:19:18 +0530

    add: marketHeader & restructuring

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,34 +1,19 @@
-import { useEthPrice } from "@components/hooks/useEthPrice";
 import { useWalletInfo } from "@components/hooks/web3";
-import { Button, BreadCrumbs } from "@components/ui/common";
+import { Button } from "@components/ui/common";
 import { getAllCourses } from "@components/ui/content/courses/fetcher";
 import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { OrderModal } from "@components/ui/order";
-import { EthRates, WalletBar } from "@components/ui/web3";
 import { useState } from "react";
+import { MarketHeader } from "@components/ui/marketplace";
 
 function Marketplace({ courses }) {
 	const [selectedCourse, setSelectedCourse] = useState(null);
-	const { account, network, canPurchaseCourse } = useWalletInfo();
-	const { eth } = useEthPrice();
-
+	const { canPurchaseCourse } = useWalletInfo();
 	return (
 		<>
-			<div className='pt-4'>
-				<WalletBar
-					address={account.data}
-					network={{
-						data: network.data,
-						target: network.target,
-						isSupported: network.isSupported,
-						hasInitialResponse: network.hasInitialResponse,
-					}}
-				/>
-				<EthRates eth={eth.data} ethPerItem={eth.perItem} />
-				<div className='flex flex-row-reverse py-4 px-4 sm:px-6 lg:px-8'>
-					<BreadCrumbs />
-				</div>
+			<div className='py-4'>
+				<MarketHeader />
 			</div>
 			<CourseList courses={courses}>
 				{(course) => (
