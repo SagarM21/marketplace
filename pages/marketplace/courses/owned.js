4adae0abc406c6ed239d5d75271966c22b0b139a
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-04 13:07:12 +0530

    created isEmpty hook for making it more easy

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -15,19 +15,18 @@ function OwnedCourses({ courses }) {
 		<>
 			<MarketHeader />
 			<section className='grid grid-cols-1'>
-				{ownedCourses.hasInitialResponse &&
-					(!ownedCourses.data || ownedCourses?.data.length === 0) && (
-						<div className='w-1/2'>
-							<Message type='warning'>
-								<div>You don't own any courses</div>
-								<Link href='/marketplace' legacyBehavior>
-									<a className='font-normal hover:underline'>
-										<i>Purchase Course</i>
-									</a>
-								</Link>
-							</Message>
-						</div>
-					)}
+				{ownedCourses.isEmpty && (
+					<div className='w-1/2'>
+						<Message type='warning'>
+							<div>You don't own any courses</div>
+							<Link href='/marketplace' legacyBehavior>
+								<a className='font-normal hover:underline'>
+									<i>Purchase Course</i>
+								</a>
+							</Link>
+						</Message>
+					</div>
+				)}
 				{ownedCourses.data?.map((course) => (
 					<OwnedCourseCard key={course.id} course={course}>
 						<Button onClick={() => router.push(`/courses/${course.slug}`)}>
