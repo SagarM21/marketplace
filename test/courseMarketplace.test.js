31cce11f29cc9ea37537d7704ad3ff95e515dbd6
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-09 16:25:59 +0530

    test activation

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -12,6 +12,7 @@ contract("CourseMarketplace", (accounts) => {
 	let _contract = null;
 	let contractOwner = null;
 	let buyer = null;
+	let courseHash = null;
 
 	before(async () => {
 		_contract = await CourseMarketplace.deployed();
@@ -20,7 +21,6 @@ contract("CourseMarketplace", (accounts) => {
 	});
 
 	describe("Purchase the new course", () => {
-		let courseHash;
 		before(async () => {
 			await _contract.purchaseCourse(courseId, proof, {
 				from: buyer,
@@ -55,6 +55,23 @@ contract("CourseMarketplace", (accounts) => {
 				course.state,
 				expectedState,
 				`Course state should be ${expectedState}!`
+			);
+		});
+	});
+
+	describe("Activate the purchased course", () => {
+		before(async () => {
+			await _contract.activateCourse(courseHash, { from: contractOwner });
+		});
+
+		it("should have 'activated' state", async () => {
+			const course = await _contract.getCourseByHash(courseHash);
+			const expectedState = 1;
+
+			assert.equal(
+				course.state,
+				expectedState,
+				"Course should have 'activated' state"
 			);
 		});
 	});
