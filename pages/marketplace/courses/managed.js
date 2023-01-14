343d36e818ee296d9777c60efa27076fc09b7156
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-14 15:41:12 +0530

    displaying filtered courses

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -131,13 +131,19 @@ export default function ManagedCourses() {
 		);
 	};
 
-	useEffect(() => {
-		console.log(filters);
-	}, [filters]);
-
 	if (!account.isAdmin) {
 		return null;
 	}
+
+	const filteredCourses = managedCourses.data
+		?.filter((course) => {
+			if (filters.state === "all") {
+				return true;
+			}
+
+			return course.state === filters.state;
+		})
+		.map((course) => renderCard(course));
 
 	return (
 		<>
@@ -154,7 +160,10 @@ export default function ManagedCourses() {
 					</div>
 				)}
 				<h1 className='text-2xl font-bold p-5'>All Courses</h1>
-				{managedCourses.data?.map((course) => renderCard(course))}
+				{filteredCourses}
+				{filteredCourses?.length === 0 && (
+					<Message type='warning'>No courses to display</Message>
+				)}
 			</section>
 		</>
 	);
