545c666a71b52674dbcd524d22717a6e9d68d438
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-16 21:45:52 +0530

    truffle goerli config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
 	contracts_build_directory: "./public/contracts",
 	networks: {
@@ -7,7 +9,21 @@ module.exports = {
 			network_id: "*",
 		},
 	},
-
+	goerli: {
+		provider: () =>
+			new HDWalletProvider({
+				mnemonic: {
+					phrase: "",
+				},
+				providerOrUrl: "https://ropsten.infura.io/v3/YOUR-PROJECT-ID",
+				addressIndex: 0,
+			}),
+		network_id: 5,
+		gas: 5500000, // Gas Limit, How much gas we are willing to spent
+		gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+		confirmations: 2, // number of blocks to wait between deployment
+		timeoutBlocks: 200, // number of blocks before deployment times out
+	},
 	compilers: {
 		solc: {
 			version: "0.8.4",
