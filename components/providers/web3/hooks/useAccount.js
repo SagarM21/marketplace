719c2c4cba488c7165203fab4b8d90df56f53864
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-18 23:19:33 +0530

    transferred ownership from deployed metamask acc to original ac

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 
 const adminAddresses = {
 	"0x6869f6d6fda46264d1b5140c73efa7d6e82c72a597c859480a40afa7622ce545": true,
+	"0x2d423c095d29afc8421774f0e8049e235472d56be167d072939adc1479e3bdff": true,
 };
 
 export const handler = (web3, provider) => () => {
