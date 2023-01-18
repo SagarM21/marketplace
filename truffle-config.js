17a784e03a7e49b1f7d384ea7b446056dabce7b9
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-18 22:47:36 +0530

    deployment to goerli

--- truffle-config.js
+++ truffle-config.js
@@ -9,21 +9,21 @@ module.exports = {
 			port: 7545,
 			network_id: "*",
 		},
-	},
-	goerli: {
-		provider: () =>
-			new HDWalletProvider({
-				mnemonic: {
-					phrase: keys.MNEMONIC,
-				},
-				providerOrUrl: `https://goerli.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
-				addressIndex: 0,
-			}),
-		network_id: 5,
-		gas: 5500000, // Gas Limit, How much gas we are willing to spent
-		gasPrice: 20000000000, // how much we are willing to spent for unit of gas
-		confirmations: 2, // number of blocks to wait between deployment
-		timeoutBlocks: 200, // number of blocks before deployment times out
+		goerli: {
+			provider: () =>
+				new HDWalletProvider({
+					mnemonic: {
+						phrase: keys.MNEMONIC,
+					},
+					providerOrUrl: `https://goerli.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+					addressIndex: 0,
+				}),
+			network_id: 5, // goerli network id
+			gas: 5500000, // Gas Limit, How much gas we are willing to spent
+			gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+			confirmations: 2, // number of blocks to wait between deployment
+			timeoutBlocks: 2000, // number of blocks before deployment times out
+		},
 	},
 	compilers: {
 		solc: {
