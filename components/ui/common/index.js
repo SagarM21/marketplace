f146dd8697f6c5d61d717ccbbb1ea2ce6488aabb
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-21 10:56:12 +0530

    spell fix

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -2,7 +2,7 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as BreadCrumbs } from "./BreadCrumbs";
-export { default as Modal } from "./Modal";
+export { default as Modal } from "./modal";
 export { default as Button } from "./Button";
 export { default as ActiveLink } from "./link";
 export { default as Message } from "./message";
