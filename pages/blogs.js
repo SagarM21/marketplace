b0c779939cd6ff9617ac24956ea06b838a62f4e6
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-01-22 16:42:37 +0530

    temporary blog pg + readme

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,11 +1,20 @@
+import { BaseLayout } from "@components/ui/layout";
 import React from "react";
 
-const blogs = () => {
+function Blogs() {
 	return (
-		<div className='flex justify-center items-center text-black font-medium bg-purple-100'>
-			In progress
-		</div>
+		<>
+			<div className='flex justify-center items-center text-black font-medium text-2xl mt-24'>
+				In progress
+			</div>
+		</>
 	);
-};
+}
 
-export default blogs;
+export default function PageInjections({ ...props }) {
+	return (
+		<BaseLayout>
+			<Blogs {...props} />
+		</BaseLayout>
+	);
+}
